d42d7ca72f9c7fb4a2c2d01b8a3e4b58ddd9dd5f
Nyx325 <omar.roman.2828@gmail.com>
2025-03-29 16:30:47 -0600

    inicio auth (roto)

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,5 +1,18 @@
 import { hash, compare } from "bcrypt";
+import { Err, Ok, Result } from "neverthrow";
+import z from "zod";
+
 import { SALT_ROUNDS } from "../config.js";
+import {
+  Cliente,
+  ClienteCriteria,
+  ClienteNuevo,
+} from "../model/entity/cliente.js";
+import { clienteToJson } from "../model/parsers/cliente.parser.js";
+import { extractErrors } from "../model/parsers/zod.parser.js";
+import Repository from "../model/repository/repository.js";
+import JsonError from "../model/value_object/json.error.js";
+import { SearchMode } from "../model/value_object/searchable.string.js";
 
 export async function hashPassword(password: string) {
   const hashedPassword = await hash(password, SALT_ROUNDS);
@@ -12,4 +25,60 @@ export async function verifyPassword(
 ) {
   const match = await compare(inputPassword, storedHash);
   return match; // true si coincide, false si no
+}
+
+export class AuthController {
+  protected parseJson: (c: Cliente) => unknown = (c) => c;
+  protected repo;
+  protected dataSchema = z.object({
+    email: z.coerce.string({ required_error: "El email es necesario" }),
+    password: z.coerce.string({ required_error: "La contraseña es necesaria" }),
+  });
+
+  constructor(
+    repo: Repository<Cliente, ClienteNuevo, number, ClienteCriteria>,
+  ) {
+    this.repo = repo;
+    this.parseJson = clienteToJson;
+  }
+
+  async login(data: unknown): Promise<Result<Cliente, JsonError[]>> {
+    const validation = this.dataSchema.safeParse(data);
+    if (!validation.success) {
+      const errors = extractErrors(validation.error);
+      return new Err(errors);
+    }
+
+    const { email, password } = validation.data;
+    const busqueda = await this.repo.getBy(
+      {
+        email: {
+          mode: SearchMode.EQUALS,
+          str: email,
+        },
+      },
+      1,
+    );
+
+    if (busqueda.result.length === 0) {
+      return new Err([
+        {
+          field: "email",
+          message: "Email no encontrado",
+        },
+      ]);
+    }
+
+    const cliente = busqueda.result[0];
+    if ((await verifyPassword(password, cliente.password)) === false) {
+      return new Err([
+        {
+          field: "password",
+          message: "Contraseña incorrecta",
+        },
+      ]);
+    }
+
+    return new Ok(cliente);
+  }
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { PORT } from "./config.js";
 import clienteRouter from "./routes/cliente.routes.js";
 import alojamientoRouter from "./routes/alojamiento.routes.js";
 import reservaRouter from "./routes/reserva.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/cliente", clienteRouter);
 app.use("/alojamiento", alojamientoRouter);
 app.use("/reserva", reservaRouter);
+app.use("/", authRouter);
 
 app.use("", (_req, res) => {
   res.status(400).json({ message: "No se encontró el endpoint" });
